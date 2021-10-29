3231bf158cc59af6cb212bd9d497c9317630f564
Femi <abiolaoshin@gmail.com>
2021-10-29 15:42:29 +0100

    Navbar toggle JS

---
+++ index.js
@@ -0,0 +1,10 @@
+var hamburger = document.getElementById('hamburger');
+var navshow = document.getElementById('menu-items')
+
+hamburger.addEventListener('click',()=>{
+    navshow.classList.toggle('nav-ul')
+});
+
+body.addEventListener('click', ()=>{
+    
+})
